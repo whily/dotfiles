1a0bce4cc30880dece3abd932057fe521f5623cc
Yujian Zhang <yujian.zhang@gmail.com>
2021-11-06 17:10:29 +0800

    Update init-lsp.el by removing outdated package company-lsp.

--- emacs/.emacs.d/lisp/init-lsp.el
+++ emacs/.emacs.d/lisp/init-lsp.el
@@ -18,12 +18,5 @@
   :custom
   (lsp-ui-sideline-ignore-duplicate t))
 
-(use-package company-lsp
-  :commands company-lsp
-  :config
-  (setq company-lsp-enable-snippet t)
-  (setq company-lsp-cache-candidates t)
-  (push 'company-lsp company-backends))
-
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
